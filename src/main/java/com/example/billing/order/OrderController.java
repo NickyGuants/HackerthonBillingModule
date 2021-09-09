0a2a3d2250309a56f55feed7ca2ee82f4818374f
NickyGuants <nicholasguantai528@gmail.com>
2021-09-09 21:57:50 +0300

    Create order controller

---
+++ src/main/java/com/example/billing/order/OrderController.java
@@ -0,0 +1,22 @@
+package com.example.billing.order;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping
+@RestController
+public class OrderController {
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/orders")
+    public List<Order>getAllOrders(){
+        return orderService.getAllOrders();
+    }
+}
