101e68c604a9046e8bd25e0a7d38733a9ac9119c
NickyGuants <nicholasguantai528@gmail.com>
2021-09-09 21:58:45 +0300

    Create patient model

--- src/main/java/com/example/billing/patient/Patient.java
+++ src/main/java/com/example/billing/patient/Patient.java
@@ -1,7 +1,11 @@
 package com.example.billing.patient;
 
+import com.example.billing.order.Order;
+
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.Period;
+import java.util.Set;
 
 @Entity
 @Table
@@ -9,26 +13,23 @@ public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-
     private Long patientId;
-
     private Boolean insured;
-
     private String name;
-
     private LocalDate dob;
-
+    private Integer insuranceNumber;
+    @Transient
     private Integer age;
-
+    @OneToMany
+    private Set<Order> Orders;
     public Patient(){
 
     }
 
-    public Patient(Long patientId, String name, LocalDate dob, Integer age, Boolean insured){
+    public Patient(Long patientId, String name, LocalDate dob, Boolean insured){
         this.patientId=patientId;
         this.name=name;
         this.dob=dob;
-        this.age=age;
         this.insured=insured;
     }
 
@@ -65,11 +66,18 @@ public class Patient {
     }
 
     public Integer getAge() {
-        return age;
+        return Period.between(this.dob, LocalDate.now()).getYears();
     }
 
     public void setAge(Integer age) {
         this.age = age;
+    }
+    public Integer getInsuranceNumber() {
+        return insuranceNumber;
+    }
+
+    public void setInsuranceNumber(Integer insuranceNumber) {
+        this.insuranceNumber = insuranceNumber;
     }
 
     @Override
