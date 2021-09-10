cc29b6b3d398a7ccabeeb56bb184e559c536e2f6
NickyGuants <nicholasguantai528@gmail.com>
2021-09-10 15:48:23 +0300

    update OrderController

--- src/main/java/com/example/billing/order/OrderController.java
+++ src/main/java/com/example/billing/order/OrderController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RequestMapping
+@RequestMapping()
 @RestController
 public class OrderController {
     private final OrderService orderService;
